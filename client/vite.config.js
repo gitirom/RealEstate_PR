f5411488b651c370ad0b65d85381081ffc26f0ab
gitirom <wadiaromdane10@gmail.com>
2023-11-10 14:21:57 +0100

    I add the SignUp functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {                          //when you call an api automatically they added the target to the current place
+        target: 'http://localhost:5000',
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
 })
