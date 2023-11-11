58892a512f7ac103225ec791a7322aae2a0fd6ce
gitirom <wadiaromdane10@gmail.com>
2023-11-11 13:06:26 +0100

    I add redux Toolkit for signin func..

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -3,15 +3,19 @@ import { useState } from "react";
 import {Link, useNavigate} from "react-router-dom";
 import { toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { useDispatch, useSelector } from "react-redux";
+import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice";
+
+
 
 
 
 
 const SignIn = () => {
     const [formData, setFormData] = useState({});
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
+    const { loading, error } = useSelector((state) => state.user);   //access this actions from the reducer
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const handleChange = (e) => {
         setFormData({
@@ -24,7 +28,7 @@ const SignIn = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            setLoading(true);
+            dispatch(signInStart());
             const res = await fetch('api/auth/signin', {
                 method: 'POST',
                 headers: {
@@ -34,18 +38,15 @@ const SignIn = () => {
             });
             const data = await res.json();
             if(data.success === false ) {     // data.success is from the file error inside the util file 
-                setError(data.message);
-                setLoading(false);
+                dispatch(signInFailure(data.message));
                 return;
             }
-            setLoading(false);
-            setError(null);
+            dispatch(signInSuccess(data));
             navigate('/');
             toast.success("User SignIn Successfully!");
             
         } catch (error) {
-            setLoading(false);
-            setError(error.message);
+            dispatch(signInFailure(error.message));
             toast.error("User SignIn Field !");
         }
     };
@@ -54,11 +55,11 @@ const SignIn = () => {
 
     return (
         <div className='p-3 max-w-lg mx-auto' >
-            <h1 className='text-3xl text-center font-semibold my-7' >Sign Ip</h1>
+            <h1 className='text-3xl text-center font-semibold my-7' >Sign In</h1>
             <form onSubmit={handleSubmit} className='flex flex-col gap-4' >
                 <input type="email" placeholder='email' id='email' onChange={handleChange} className='border p-3 rounded-lg gap-4' />
                 <input type="password" placeholder='password' id='password' onChange={handleChange} className='border p-3 rounded-lg gap-4' />
-                <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-80 disabled:opacity-80  ' >{loading ? 'Loading...' : 'Sign Ip'}</button>
+                <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-80 disabled:opacity-80  ' >{loading ? 'Loading...' : 'Sign In'}</button>
             </form>
             <div className="flex gap-2 mt-5">
                 <p>Don't Have an account?</p>
