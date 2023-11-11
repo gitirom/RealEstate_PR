49950d46f1d0f64998f445db3b9413d5842ccf33
gitirom <wadiaromdane10@gmail.com>
2023-11-11 19:18:09 +0100

    add a persist functionality to the store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({user: userReducer})
+
+const persistConfig = {    //persist it's just for getting store the user in the locale storage and not getting lose the current user
+    key: 'root',
+    storage,
+    version: 1,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
 
 export const store = configureStore({
-    reducer: { user: userReducer },
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) => 
     getDefaultMiddleware({
         serializableCheck: false,             //to prevent eny error from variables
     }),
 });
+
+
+export const persistor = persistStore(store);
